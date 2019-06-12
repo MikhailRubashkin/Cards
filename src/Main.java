d9c70a98a94b9a064ffb7dbe4ec9355cb17afee5
MikhailRubashkin <pristijpribor@mail.ru>
2019-06-12 12:47:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+
+//Написать программу, эмулирующую выдачу случайной карты из колоды в 52 карты.
+
+
+public class Main {
+
+
+
+
+        public static void main(String[] args) {
+
+
+            String[] suits = {
+                    "Spades", "Diamonds", "Hearts", "Clubs"
+            };
+
+            String[] rank = {
+                    "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                    "Jack", "Queen", "King", "Ace"
+            };
+
+            int n = suits.length * rank.length;
+
+
+            String[] deck = new String[n];
+            for (int i = 0; i < rank.length; i++) {
+                for (int j = 0; j < suits.length; j++) {
+                    deck[suits.length*i + j] = rank[i] + " " + suits[j];
+                }
+            }
+
+
+            for (int i = 0; i < n; i++) {
+                int r = i + (int) (Math.random() * (n-i));
+                String temp = deck[r];
+                deck[r] = deck[i];
+                deck[i] = temp;
+
+            }
+
+
+            for (int i = 0; i < 1; i++) {
+                System.out.println(deck[i]);
+
+            }
+        }
+
+    }
+
+
