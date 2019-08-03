c025c45883677acc098544a803b13485a69fe279
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2019-08-03 23:16:45 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,34 @@
 //Написать программу, эмулирующую выдачу случайной карты из колоды в 52 карты.
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+
+
+
 public class Main {
 
+    public static void main ( String[] args ){
+        Random randomGenerator;
+        Random randomGenerator2;
+
+        List<String> strings = Arrays.asList ("Spades ", "Diamonds ", "Hearts ", "Clubs ");
+        randomGenerator = new Random ( );
+        String randomInt = strings.get (randomGenerator.nextInt (strings.size ( )));
+        System.out.print (randomInt);
+
+        List<String> strings2 = Arrays.asList ("2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ", "10 ",
+                                               "Jack ", "Queen ", "King ", "Ace ");
+        randomGenerator2 = new Random ( );
+        String randomInt2 = strings2.get (randomGenerator2.nextInt (strings2.size ( )));
+        System.out.print (randomInt2);
+    }
+}
 
 
-
-        public static void main(String[] args) {
+        /*public static void main(String[] args) {
 
 
             String[] suits = {
@@ -43,8 +65,7 @@ public class Main {
                 System.out.println(deck[i]);
 
             }
-        }
+        }*/
 
-    }
 
 
